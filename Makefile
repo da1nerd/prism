5fe308f3e927e05778a482669c6e0732d10205e6
Joel Lonbeck <joel@neutrinographics.com>
2018-08-19 23:43:57 -0700

    added opengl library

--- Makefile
+++ Makefile
@@ -1,18 +1,25 @@
 build: directories
 	@crystal build src/prism.cr -o bin/debug/prism
 
-release: directories
+release: clean directories docs test
 	@crystal build src/prism.cr -o bin/release/prism --release
 
-run: build
-	./bin/debug/prism
+run:
+	@crystal src/prism.cr
 
 directories:
 	@mkdir -p bin
 	@mkdir -p bin/debug
 	@mkdir -p bin/release
 
+docs:
+	@crystal docs src/prism.cr
+
+test:
+	@crystal spec
+
 clean:
 	@rm -rf bin
+	@rm -rf docs
 
 .PHONY: build
--- src/prism.cr
+++ src/prism.cr
@@ -1,6 +1,47 @@
+require "lib_gl"
+require "crystglfw"
+
+include CrystGLFW
+
 # TODO: Write documentation for `Prism`
 module Prism
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  CrystGLFW.run do
+    # Request a specific version of OpenGL in core profile mode with forward compatibility.
+    hints = {
+      CrystGLFW::Window::HintLabel::ContextVersionMajor => 3,
+      CrystGLFW::Window::HintLabel::ContextVersionMinor => 3,
+      CrystGLFW::Window::HintLabel::OpenGLForwardCompat => true,
+      CrystGLFW::Window::HintLabel::OpenGLProfile => CrystGLFW::OpenGLProfile::Core
+      # :context_version_major => 3,
+      # :context_version_minor => 3,
+      # :opengl_forward_compat => true,
+      # :opengl_profile => :opengl_core_profile
+    }
+
+    # Create a new window.
+    window = Window.new(title: "LibGL Rocks!", hints: hints)
+
+    # Make the window the current OpenGL context.
+    window.make_context_current
+
+    until window.should_close?
+      CrystGLFW.poll_events
+
+      # Use the timer to generate color values.
+      time_value = CrystGLFW.time
+      red_value = Math.sin(time_value).abs
+      green_value = Math.cos(time_value).abs
+      blue_value = Math.tan(time_value).abs
+
+      # Clear the window with the generated color.
+      LibGL.clear_color(red_value, green_value, blue_value, 1.0)
+      LibGL.clear(LibGL::COLOR_BUFFER_BIT)
+
+      window.swap_buffers
+    end
+
+    window.destroy
+  end
 end
