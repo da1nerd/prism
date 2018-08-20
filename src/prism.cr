adb09ffa4b89269a70c08f5127a27053df3b5359
Joel Lonbeck <joel@neutrinographics.com>
2018-08-20 08:44:35 -0700

    fixed binding to init

--- src/prism.cr
+++ src/prism.cr
@@ -18,14 +18,15 @@ module Prism
   VERSION = "0.1.0"
 
   argv = ARGV.map(&.to_unsafe).to_unsafe
-  Glut.init(ARGV.size, argv)
-  # Glut.init_display_mode(Glut::SINGLE)
-  # Glut.init_window_size(500, 500)
-  # Glut.init_window_position(100, 100)
-  # Glut.create_window("OpenGL - Creating a triangle")
-  #
-  # Glut.display_func(drawTriangle)
-  # Glut.main_loop()
+  size = ARGV.size
+  Glut.init(pointerof(size), argv)
+  Glut.init_display_mode(Glut::SINGLE)
+  Glut.init_window_size(500, 500)
+  Glut.init_window_position(100, 100)
+  Glut.create_window("OpenGL - Creating a triangle")
+
+  Glut.display_func(drawTriangle)
+  Glut.main_loop()
 end
 
 @[Link("glut")]
@@ -33,7 +34,7 @@ lib Glut
   # GLUT API macro definitions -- the display mode definitions
   SINGLE = 0x0000
 
-  fun init = glutInit(argc : Int32, argv : UInt8**) : Void
+  fun init = glutInit(argc : Int32*, argv : UInt8**) : Void
   fun init_display_mode = glutInitDisplayMode(displayMode : UInt32) : Void
   fun init_window_size = glutInitWindowSize(width: Int32, height: Int32) : Void
   fun init_window_position = glutInitWindowPosition(x: Int32, y : Int32) : Void
