5313b7a02296359c45cb2f5d8b95f5ab531801d9
Joel Lonbeck <joel@neutrinographics.com>
2018-08-21 02:58:30 -0700

    added keyboard and mouse handlers

--- samples/blank_window.cr
+++ samples/blank_window.cr
@@ -7,19 +7,32 @@ module Example
 
 
   render = ->( x : Void) {
-    # LibGL.color3f(1.0, 1.0, 1.0);
-    LibGL.ortho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    LibGL.clear(LibGL::COLOR_BUFFER_BIT | LibGL::DEPTH_BUFFER_BIT)
 
     LibGL.begin(LibGL::TRIANGLES);
-    LibGL.vertex3f(-0.7, 0.7, 0);
-    LibGL.vertex3f(0.7, 0.7, 0);
-    LibGL.vertex3f(0, -1, 0);
+    LibGL.color3f(1, 0, 0);
+    LibGL.vertex2f(-0.5, -0.5);
+    LibGL.color3f(0, 1, 0);
+    LibGL.vertex2f(0.5, -0.5);
+    LibGL.color3f(0, 0, 1);
+    LibGL.vertex2f(0, 0.5);
+
     LibGL.end();
     puts "hello world"
     LibGL.flush();
   }
 
+  keyboard = ->(c : UInt8, x : Int32, y : Int32) {
+    if c == 27
+      exit()
+    end
+  }
+
+  mouse = ->(button : Int32, state : Int32, x : Int32, y : Int32) {
+
+  }
+
   # TODO: only create a blank window
-  Prism::Window.create_window(800, 600, "Sample Window", render)
+  window = Prism::Window.new(800, 600, "Sample Window", render, keyboard, mouse)
 
 end
--- src/prism/window.cr
+++ src/prism/window.cr
@@ -3,7 +3,14 @@ require "lib_glut"
 module Prism
   class Window
 
-    def self.create_window(width : Int32, height : Int32, title : String, render : Void -> Void)
+    def initialize(
+        width : Int32,
+        height : Int32,
+        title : String,
+        render : Proc(Void, Nil),
+        keyboard : Proc(UInt8, Int32, Int32, Nil),
+        mouse : Proc(Int32, Int32, Int32, Int32, Nil)
+      )
       args = [] of String
       argv = args.map(&.to_unsafe).to_unsafe
       size = args.size
@@ -11,22 +18,33 @@ module Prism
       LibGlut.init_display_mode(LibGlut::SINGLE)
       LibGlut.init_window_size(width, height)
       LibGlut.init_window_position(100, 100)
-      LibGlut.create_window(title)
+      @title = title
+      @id = LibGlut.create_window(title)
 
       LibGlut.display_func(render)
-      
+      LibGlut.keyboard_func(keyboard)
+      LibGlut.mouse_func(mouse)
+
       LibGlut.main_loop()
     end
 
-    def self.isCloseRequested : Boolean
+    def on_keyboard
+      # TODO: impliment this so we don't have to pass in the keyboard handler to the constructor
+    end
+
+    def on_mouse
+      # TODO: implement this
+    end
+
+    def isCloseRequested : Boolean
 
     end
 
-    def self.getWidth : Int32
+    def getWidth : Int32
       return LibGlut.get(LibGlut::WINDOW_WIDTH)
     end
 
-    def self.getHeight : Int32
+    def getHeight : Int32
       return LibGlut.get(LibGlut::WINDOW_HEIGHT)
     end
 
