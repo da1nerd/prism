10eac79ba52e8af9ed234d1436612e03e7e41ec9
Joel Lonbeck <joel@neutrinographics.com>
2018-08-20 22:46:56 -0700

    added module method

--- src/prism.cr
+++ src/prism.cr
@@ -5,28 +5,34 @@ require "./lib_glut"
 module Prism
   VERSION = "0.1.0"
 
-  drawTriangle = ->( x : Void) {
-    # LibGL.color3f(1.0, 1.0, 1.0);
-    LibGL.ortho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+  # TODO: we must build this.
+  class Window
+  end
 
-    LibGL.begin(LibGL::TRIANGLES);
-    LibGL.vertex3f(-0.7, 0.7, 0);
-    LibGL.vertex3f(0.7, 0.7, 0);
-    LibGL.vertex3f(0, -1, 0);
-    LibGL.end();
+  def start
+    drawTriangle = ->( x : Void) {
+      # LibGL.color3f(1.0, 1.0, 1.0);
+      LibGL.ortho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
 
-    LibGL.flush();
-  }
+      LibGL.begin(LibGL::TRIANGLES);
+      LibGL.vertex3f(-0.7, 0.7, 0);
+      LibGL.vertex3f(0.7, 0.7, 0);
+      LibGL.vertex3f(0, -1, 0);
+      LibGL.end();
 
-  argv = ARGV.map(&.to_unsafe).to_unsafe
-  size = ARGV.size
-  LibGlut.init(pointerof(size), argv)
-  LibGlut.init_display_mode(LibGlut::SINGLE)
-  LibGlut.init_window_size(500, 500)
-  LibGlut.init_window_position(100, 100)
-  LibGlut.create_window("OpenGL - Creating a triangle")
+      LibGL.flush();
+    }
 
-  LibGlut.display_func(drawTriangle)
+    argv = ARGV.map(&.to_unsafe).to_unsafe
+    size = ARGV.size
+    LibGlut.init(pointerof(size), argv)
+    LibGlut.init_display_mode(LibGlut::SINGLE)
+    LibGlut.init_window_size(500, 500)
+    LibGlut.init_window_position(100, 100)
+    LibGlut.create_window("OpenGL - Creating a triangle")
 
-  LibGlut.main_loop()
+    LibGlut.display_func(drawTriangle)
+
+    LibGlut.main_loop()
+  end
 end
