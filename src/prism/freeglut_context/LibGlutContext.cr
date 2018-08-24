94a573f9f04e75047fd8830c9854ba44453fa5b3
Joel Lonbeck <joel@neutrinographics.com>
2018-08-24 03:06:42 -0700

    organized lib

--- src/prism/freeglut_context/LibGlutContext.cr
+++ src/prism/freeglut_context/LibGlutContext.cr
@@ -1,5 +1,5 @@
-@[Link(ldflags: "#{__DIR__}/libfreeglut_closure.a")]
-lib LibGlutClosure
+@[Link(ldflags: "#{__DIR__}/lib/freeglut_context.a")]
+lib LibGlutContext
   fun display_func = glutDisplayFuncWithContext(callback : Void* -> Void, data : Void*) : Void
   fun close_func = glutCloseFuncWithContext(callback : Void* -> Void, data : Void*) : Void
 end
