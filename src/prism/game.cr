507f26956d43cd0de34e7b6b9b27e100b3c774f3
Joel Lonbeck <joel@neutrinographics.com>
2018-08-24 17:37:23 -0700

    debugging

--- src/prism/game.cr
+++ src/prism/game.cr
@@ -51,16 +51,16 @@ module Prism
     def render
       @mesh.draw
       # LibGL.clear(LibGL::COLOR_BUFFER_BIT | LibGL::DEPTH_BUFFER_BIT)
-      #
-      # LibGL.begin(LibGL::TRIANGLES);
-      # LibGL.color3f(1, 0, 0);
-      # LibGL.vertex2f(-0.5, -0.5);
-      # LibGL.color3f(0, 1, 0);
-      # LibGL.vertex2f(0.5, -0.5);
-      # LibGL.color3f(0, 0, 1);
-      # LibGL.vertex2f(0, 0.5);
-      #
-      # LibGL.end();
+      # #
+      LibGL.begin(LibGL::TRIANGLES);
+      LibGL.color3f(1, 0, 0);
+      LibGL.vertex2f(-0.5, -0.5);
+      LibGL.color3f(0, 1, 0);
+      LibGL.vertex2f(0.5, -0.5);
+      LibGL.color3f(0, 0, 1);
+      LibGL.vertex2f(0, 0.5);
+
+      LibGL.end();
       # LibGL.flush();
     end
 
