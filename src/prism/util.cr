03bffd9b30a4a56e07472eaf63fca962ffcb2cd1
Joel Lonbeck <joel@neutrinographics.com>
2018-08-26 04:11:32 -0700

    fixing mesh'

--- src/prism/util.cr
+++ src/prism/util.cr
@@ -2,8 +2,16 @@ module Prism
 
   class Util
     def self.create_flipped_buffer(verticies : Array(Vertex))
-      # TODO: create array compatible with open gl
-      return verticies
+      buffer = [] of Float32
+
+      verticies.each do |v|
+        buffer.push(v.pos.x)
+        buffer.push(v.pos.y)
+        buffer.push(v.pos.z)
+      end
+
+      buffer.reverse!
+      return buffer
     end
   end
 
