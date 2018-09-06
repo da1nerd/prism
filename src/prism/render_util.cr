d9004ba701159f6613fc1f7a9c0a879a8a0f73e9
Joel Lonbeck <joel@neutrinographics.com>
2018-09-06 10:34:25 -0700

    draw 3d triangle

--- src/prism/render_util.cr
+++ src/prism/render_util.cr
@@ -14,7 +14,7 @@ module Prism
 
       LibGL.front_face(LibGL::CW)
       LibGL.cull_face(LibGL::BACK)
-      LibGL.enable(LibGL::CULL_FACE)
+      # LibGL.enable(LibGL::CULL_FACE)
       LibGL.enable(LibGL::DEPTH_TEST)
 
       # TODO: Depth clamp for later
