211db699979fb20be74530753795a8dec7df39dd
Joel Lonbeck <joel@neutrinographics.com>
2018-09-06 01:32:59 -0700

    added rotation

--- src/prism/transform.cr
+++ src/prism/transform.cr
@@ -7,12 +7,16 @@ module Prism
   class Transform
 
     @translation : Vector3f
+    @rotation : Vector3f
 
     getter translation
     setter translation
+    getter rotation
+    setter rotation
 
     def initialize()
       @translation = Vector3f.new(0, 0, 0)
+      @rotation = Vector3f.new(0, 0, 0)
     end
 
     # additional setter in case I don't want to create a vector before hand.
@@ -20,10 +24,18 @@ module Prism
       @translation = Vector3f.new(x, y, z)
     end
 
+    def rotation(x : Float32, y : Float32 , z : Float32)
+      @rotation = Vector3f.new(x, y, z)
+    end
+
     def get_transformation : Matrix4f
-      translationMatrix = Matrix4f.new
-      translationMatrix.init_translation(@translation.x, @translation.y, @translation.z)
-      return translationMatrix
+      trans = Matrix4f.new
+      trans.init_translation(@translation.x, @translation.y, @translation.z)
+
+      rot = Matrix4f.new
+      rot.init_rotation(@rotation.x, @rotation.y, @rotation.z)
+
+      return trans * rot
     end
 
   end
