3f4f325d2f4cde9c5917fe25230d7c34d85e111c
Joel Lonbeck <joel@neutrinographics.com>
2018-09-18 21:37:44 -0700

    added specular reflection

--- src/prism/material.cr
+++ src/prism/material.cr
@@ -5,10 +5,18 @@ module Prism
 
   class Material
 
-    getter texture, color
-    setter texture, color
+    getter texture, color, specular_intensity, specular_exponent
+    setter texture, color, specular_intensity, specular_exponent
 
-    def initialize(@texture : Texture, @color : Vector3f)
+    def initialize(texture : Texture)
+      initialize(texture, Vector3f.new(1,1,1))
+    end
+
+    def initialize(texture : Texture, color : Vector3f)
+      initialize(texture, color, 2, 32)
+    end
+
+    def initialize(@texture : Texture, @color : Vector3f, @specular_intensity : Float32, @specular_exponent : Float32)
     end
 
   end
