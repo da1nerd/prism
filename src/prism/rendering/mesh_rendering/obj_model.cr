370905ba8c87b3f7b33620ead05ffef98d2bd927
Joel Lonbeck <joel@neutrinographics.com>
2018-10-09 14:26:14 -0700

    improved normal calculations
    Former-commit-id: 9cf88b2a057025c1e23cf6fd5115d54f09bd892d

--- src/prism/rendering/mesh_rendering/obj_model.cr
+++ src/prism/rendering/mesh_rendering/obj_model.cr
@@ -14,8 +14,6 @@ module Prism
     @has_tex_coords : Bool
     @has_normals : Bool
 
-    # getter positions, tex_coords, normals, indicies
-
     def initialize(file_name : String)
       @positions = [] of Vector3f
       @tex_coords = [] of Vector2f
@@ -50,9 +48,11 @@ module Prism
 
     def to_indexed_model : IndexedModel
       result = IndexedModel.new
-      index_map = {} of OBJIndex => Int32
+      normal_model = IndexedModel.new
+      result_index_map = {} of OBJIndex => Int32
+      normal_index_map = {} of Int32 => Int32
+      index_map = {} of Int32 => Int32
 
-      current_vertex_index = 0
       0.upto(@indicies.size - 1) do |i|
         current_index = @indicies[i]
         current_position = @positions[current_index.vertex_index]
@@ -67,35 +67,51 @@ module Prism
           current_normal = @normals[current_index.normal_index]
         end
 
-        previous_vertex_index = -1
-
-        if index_map.has_key?(current_index)
-          previous_vertex_index = index_map[current_index]
+        # get the mesh
+        model_vertex_index = -1
+        if result_index_map.has_key?(current_index)
+          model_vertex_index = result_index_map[current_index]
         end
 
-        if previous_vertex_index == -1
-          index_map[current_index] = current_vertex_index
+        if model_vertex_index == -1
+          model_vertex_index = result.positions.size
+          result_index_map[current_index] = model_vertex_index
 
           result.positions.push(current_position)
           result.tex_coords.push(current_tex_coord)
-          result.normals.push(current_normal)
-          result.indicies.push(current_vertex_index)
-          current_vertex_index += 1
-        else
-          result.indicies.push(previous_vertex_index)
+          if @has_normals
+            result.normals.push(current_normal)
+          end
         end
 
-        # if previous_vertex_index == -1
-        #   index_map[i] = current_vertex_index
-        #
-        #   result.positions.push(current_position)
-        #   result.tex_coords.push(current_tex_coord)
-        #   result.normals.push(current_normal)
-        #   result.indicies.push(current_vertex_index)
-        #   current_vertex_index += 1
-        # else
-        #   result.indicies.push(index_map[previous_vertex_index])
-        # end
+        # get the normals
+        normal_model_index = -1
+        if normal_index_map.has_key?(current_index.vertex_index)
+          normal_model_index = normal_index_map[current_index.vertex_index]
+        end
+
+        if normal_model_index == -1
+          normal_model_index = normal_model.positions.size
+          normal_index_map[current_index.vertex_index] = normal_model_index
+
+          normal_model.positions.push(current_position)
+          normal_model.tex_coords.push(current_tex_coord)
+          normal_model.normals.push(current_normal)
+        end
+
+        result.indicies.push(model_vertex_index)
+        normal_model.indicies.push(normal_model_index)
+
+        # map normals to the model
+        index_map[model_vertex_index] = normal_model_index
+      end
+
+      unless @has_normals
+        normal_model.calc_normals
+
+        0.upto(result.positions.size - 1) do |i|
+          result.normals.push(normal_model.normals[index_map[i]])
+        end
       end
 
       return result
