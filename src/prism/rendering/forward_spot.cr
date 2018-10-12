ad18b52a18005751b754cca861ee9a4d682598ea
Joel Lonbeck <joel@neutrinographics.com>
2018-10-11 23:36:20 -0700

    fixed bug in forward directional

--- src/prism/rendering/forward_spot.cr
+++ src/prism/rendering/forward_spot.cr
@@ -23,7 +23,7 @@ module Prism
       set_uniform("MVP", projected_matrix)
 
       set_uniform("specularIntensity", material.get_float("specularIntensity"))
-      set_uniform("specularExponent", material.get_float("specularPower"))
+      set_uniform("specularPower", material.get_float("specularPower"))
       set_uniform("C_eyePos", rendering_engine.main_camera.transform.get_transformed_pos)
 
       set_uniform_spot_light("spotLight", rendering_engine.active_light.as(SpotLight))
