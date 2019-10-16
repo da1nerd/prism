d4baffbe6f5c2ad9d1c816e4ddb8b8d2daf7de64
Joel <joel@neutrinographics.com>
2019-10-17 01:26:24 +0700

    added box shape
    Former-commit-id: 92be54e11eb36a4e64c7c3259e3f58946e1b3e36

--- src/prism/shapes/plain.cr
+++ src/prism/shapes/plain.cr
@@ -12,19 +12,19 @@ module Prism
                 field_depth = 1.0f32
                 field_width = 1.0f32
 
-                @verticies = [
-                    Vertex.new(Vector3f.new(-field_width, 0, -field_depth), Vector2f.new(0, 0)),
-                    Vertex.new(Vector3f.new(-field_width, 0, field_depth * @depth), Vector2f.new(0, 1)),
-                    Vertex.new(Vector3f.new(field_width * @width, 0, -field_depth), Vector2f.new(1, 0)),
-                    Vertex.new(Vector3f.new(field_width * @width, 0, field_depth * @depth), Vector2f.new(1, 1))
+                verticies = [
+                    Vertex.new(Vector3f.new(0, 0, 0), Vector2f.new(0, 0)),
+                    Vertex.new(Vector3f.new(0, 0, @depth), Vector2f.new(0, 1)),
+                    Vertex.new(Vector3f.new(@width, 0, 0), Vector2f.new(1, 0)),
+                    Vertex.new(Vector3f.new(@width, 0, @depth), Vector2f.new(1, 1))
                 ]
 
-                @indicies = Array(LibGL::Int) {
+                indicies = Array(LibGL::Int) {
                     0, 1, 2,
                     2, 1, 3
                 }
                 
-                @mesh = Mesh.new(@verticies, @indicies, true)
+                @mesh = Mesh.new(verticies, indicies, true)
             end
         end
     end
