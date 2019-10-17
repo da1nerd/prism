7e478dd8306f339f12e438ce08cc2c3e4755aa5c
Joel <joel@neutrinographics.com>
2019-10-17 15:30:43 +0700

    added auto sync to camera aspect ratio
    Former-commit-id: 95b9f7068e07c177cce985fff55637f0ac7f9890

--- src/prism/components/camera.cr
+++ src/prism/components/camera.cr
@@ -5,6 +5,7 @@ require "./game_component"
 module Prism
   class Camera < GameComponent
     @projection : Matrix4f
+    @sync_aspect_ratio : Bool
 
     # Creates a camera with default values
     def initialize
@@ -14,7 +15,22 @@ module Prism
       initialize(field_of_view, aspect_ratio, 0.01f32, 1000f32)
     end
 
+    # Controls whether or not the camera should automatically sync
+    # it's aspect ratio with the window size.
+    def sync_aspect_ratio=(@sync_aspect_ratio : Bool)
+    end
+
+    def input(delta : Float32, input : Input)
+      super
+      # keep the aspect ratio in sync with the window
+      if @sync_aspect_ratio
+        size = input.window_size
+        self.aspect = (size[:width] / size[:height]).to_f32
+      end
+    end
+
     def initialize(@fov : Float32, @aspect : Float32, @z_near : Float32, @z_far : Float32)
+      @sync_aspect_ratio = true
       @projection = Matrix4f.new.init_perspective(@fov, @aspect, @z_near, @z_far)
     end
 
