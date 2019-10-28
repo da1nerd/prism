879353ab4873e5a7593b5783c593660561c00900
Joel <joel@neutrinographics.com>
2019-10-28 23:24:27 +0700

    removed game code

--- samples/main.cr
+++
@@ -1,7 +0,0 @@
-require "../src/prism/**"
-require "./game"
-require "lib_gl"
-
-#  Example creating a window
-engine = CoreEngine.new(800, 600, 60.0, "3D Game Engine", TestGame.new())
-engine.start
--- samples/obstacle.cr
+++
@@ -1,4 +0,0 @@
-module Obstacle
-    abstract def position : Vector3f
-    abstract def size : Vector3f
-end
