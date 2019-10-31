803fbe9a4ab4eb0a4ec70b57b88b2ca9a58c629d
Joel <joel@neutrinographics.com>
2019-10-31 18:06:21 +0700

    added sprites

--- src/prism/shapes/box.cr
+++ src/prism/shapes/box.cr
@@ -3,7 +3,17 @@ require "./shape"
 module Prism
     module Shapes
         class Box < Shape
+
             def initialize(@size : Float32)
+                initialize(@size, {
+                    bottom_left:  Vector2f.new(0, 0),
+                    top_left:     Vector2f.new(0, 1),
+                    bottom_right: Vector2f.new(1, 0),
+                    top_right:    Vector2f.new(1, 1)
+                })
+            end
+
+            def initialize(@size : Float32, @texture_coords : SpriteCoords)
                 super()
 
                 field_depth = 1.0f32
@@ -11,40 +21,40 @@ module Prism
 
                 verticies = [
                     # bottom
-                    Vertex.new(Vector3f.new(0, 0, 0), Vector2f.new(0, 0)),
-                    Vertex.new(Vector3f.new(0, 0, @size), Vector2f.new(0, 1)),
-                    Vertex.new(Vector3f.new(@size, 0, @size), Vector2f.new(1, 1)),
-                    Vertex.new(Vector3f.new(@size, 0, 0), Vector2f.new(1, 0)),
+                    Vertex.new(Vector3f.new(0, 0, 0), @texture_coords[:bottom_left]),
+                    Vertex.new(Vector3f.new(0, 0, @size), @texture_coords[:bottom_right]),
+                    Vertex.new(Vector3f.new(@size, 0, @size), @texture_coords[:top_right]),
+                    Vertex.new(Vector3f.new(@size, 0, 0), @texture_coords[:top_left]),
 
                     # top
-                    Vertex.new(Vector3f.new(0, @size, 0), Vector2f.new(0, 0)),
-                    Vertex.new(Vector3f.new(0, @size, @size), Vector2f.new(0, 1)),
-                    Vertex.new(Vector3f.new(@size, @size, @size), Vector2f.new(1, 1)),
-                    Vertex.new(Vector3f.new(@size, @size, 0), Vector2f.new(1, 0)),
+                    Vertex.new(Vector3f.new(0, @size, 0), @texture_coords[:bottom_left]),
+                    Vertex.new(Vector3f.new(0, @size, @size), @texture_coords[:bottom_right]),
+                    Vertex.new(Vector3f.new(@size, @size, @size), @texture_coords[:top_right]),
+                    Vertex.new(Vector3f.new(@size, @size, 0), @texture_coords[:top_left]),
 
                     # back
-                    Vertex.new(Vector3f.new(0, 0, @size), Vector2f.new(0, 0)),
-                    Vertex.new(Vector3f.new(0, @size, @size), Vector2f.new(0, 1)),
-                    Vertex.new(Vector3f.new(@size, @size, @size), Vector2f.new(1, 1)),
-                    Vertex.new(Vector3f.new(@size, 0, @size), Vector2f.new(1, 0)),
+                    Vertex.new(Vector3f.new(0, 0, @size), @texture_coords[:bottom_left]),
+                    Vertex.new(Vector3f.new(0, @size, @size), @texture_coords[:bottom_right]),
+                    Vertex.new(Vector3f.new(@size, @size, @size), @texture_coords[:top_right]),
+                    Vertex.new(Vector3f.new(@size, 0, @size), @texture_coords[:top_left]),
 
                     # front
-                    Vertex.new(Vector3f.new(0, 0, 0), Vector2f.new(0, 0)),
-                    Vertex.new(Vector3f.new(0, @size, 0), Vector2f.new(0, 1)),
-                    Vertex.new(Vector3f.new(@size, @size, 0), Vector2f.new(1, 1)),
-                    Vertex.new(Vector3f.new(@size, 0, 0), Vector2f.new(1, 0)),
+                    Vertex.new(Vector3f.new(0, 0, 0), @texture_coords[:bottom_left]),
+                    Vertex.new(Vector3f.new(0, @size, 0), @texture_coords[:bottom_right]),
+                    Vertex.new(Vector3f.new(@size, @size, 0), @texture_coords[:top_right]),
+                    Vertex.new(Vector3f.new(@size, 0, 0), @texture_coords[:top_left]),
 
                     # left
-                    Vertex.new(Vector3f.new(0, 0, 0), Vector2f.new(0, 0)),
-                    Vertex.new(Vector3f.new(0, 0, @size), Vector2f.new(0, 1)),
-                    Vertex.new(Vector3f.new(0, @size, @size), Vector2f.new(1, 1)),
-                    Vertex.new(Vector3f.new(0, @size, 0), Vector2f.new(1, 0)),
+                    Vertex.new(Vector3f.new(0, 0, 0), @texture_coords[:bottom_left]),
+                    Vertex.new(Vector3f.new(0, 0, @size), @texture_coords[:bottom_right]),
+                    Vertex.new(Vector3f.new(0, @size, @size), @texture_coords[:top_right]),
+                    Vertex.new(Vector3f.new(0, @size, 0), @texture_coords[:top_left]),
 
                     # right
-                    Vertex.new(Vector3f.new(@size, 0, 0), Vector2f.new(0, 0)),
-                    Vertex.new(Vector3f.new(@size, 0, @size), Vector2f.new(0, 1)),
-                    Vertex.new(Vector3f.new(@size, @size, @size), Vector2f.new(1, 1)),
-                    Vertex.new(Vector3f.new(@size, @size, 0), Vector2f.new(1, 0))
+                    Vertex.new(Vector3f.new(@size, 0, 0), @texture_coords[:bottom_left]),
+                    Vertex.new(Vector3f.new(@size, 0, @size), @texture_coords[:bottom_right]),
+                    Vertex.new(Vector3f.new(@size, @size, @size), @texture_coords[:top_right]),
+                    Vertex.new(Vector3f.new(@size, @size, 0), @texture_coords[:top_left])
                 ]
 
                 indicies = Array(LibGL::Int) {
