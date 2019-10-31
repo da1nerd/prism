79c0ee54a2989c7976ff5f626c96ff0b33e1e7c9
Joel <joel@neutrinographics.com>
2019-10-31 18:06:21 +0700

    added sprites
    Former-commit-id: 38bebfc2dd12af29b635616ab612b2f2e7228e73

--- src/prism/rendering/spritemap.cr
+++ src/prism/rendering/spritemap.cr
@@ -2,14 +2,21 @@ module Prism
   # Simplifies generating square sprite texture coordinates
   # by mapping the actual coordinates to a grid system.
   class Spritemap
+
+    @material : Material?
+
+    # Creates a new map of *width* sprites by *height* sprites.
+    def initialize(@width : UInt8, @height : UInt8, @material : Material)
+    end
+
     # Creates a new map of *width* sprites by *height* sprites.
     def initialize(@width : UInt8, @height : UInt8)
-    end
+    end    
 
     # Returns the corners of the sprite.
     #
     # Each corner is labeled accordingly: top_left, top_right, bottom_right, bottom_left
-    def get(x : UInt8, y : UInt8) : NamedTuple(bottom_left: Vector2f, bottom_right: Vector2f, top_right: Vector2f, top_left: Vector2f)
+    def get(x : UInt8, y : UInt8) : SpriteCoords
       left = (x.to_f32) / @width.to_f32
       right = (x.to_f32 + 1) / @width.to_f32
       top = (y.to_f32) / @height.to_f32
@@ -20,6 +27,11 @@ module Prism
         top_right:    Vector2f.new(right, top),
         top_left:     Vector2f.new(left, top),
       }
+    end
+
+    # Returns the sprite at the given coordinates.
+    def get_sprite(x : UInt8, y : UInt8) : Sprite
+      Sprite.new(self.get(x, y), @material)
     end
   end
 end
--- src/prism/shapes/plain.cr
+++ src/prism/shapes/plain.cr
@@ -6,17 +6,27 @@ require "lib_gl"
 module Prism
     module Shapes
         class Plain < Shape
+
             def initialize(@width : Float32, @depth : Float32)
+                initialize(@width, @depth, {
+                    bottom_left:  Vector2f.new(0, 0),
+                    top_left:     Vector2f.new(0, 1),
+                    bottom_right: Vector2f.new(1, 0),
+                    top_right:    Vector2f.new(1, 1)
+                })
+            end
+
+            def initialize(@width : Float32, @depth : Float32, @texture_coords : SpriteCoords)
                 super()
 
                 field_depth = 1.0f32
                 field_width = 1.0f32
 
                 verticies = [
-                    Vertex.new(Vector3f.new(0, 0, 0), Vector2f.new(0, 0)),
-                    Vertex.new(Vector3f.new(0, 0, @depth), Vector2f.new(0, 1)),
-                    Vertex.new(Vector3f.new(@width, 0, 0), Vector2f.new(1, 0)),
-                    Vertex.new(Vector3f.new(@width, 0, @depth), Vector2f.new(1, 1))
+                    Vertex.new(Vector3f.new(0, 0, 0), @texture_coords[:bottom_left]),#Vector2f.new(0, 0)),
+                    Vertex.new(Vector3f.new(0, 0, @depth), @texture_coords[:top_left]),#Vector2f.new(0, 1)),
+                    Vertex.new(Vector3f.new(@width, 0, 0), @texture_coords[:bottom_right]),#Vector2f.new(1, 0)),
+                    Vertex.new(Vector3f.new(@width, 0, @depth), @texture_coords[:top_right]),#Vector2f.new(1, 1))
                 ]
 
                 indicies = Array(LibGL::Int) {
