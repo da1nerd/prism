89a32e4a414c531f2d49301002157341bc1d9455
Joel <joel@neutrinographics.com>
2020-03-29 20:12:27 +0700

    spritemap updates
    Former-commit-id: afe79cbbdc73b0b52b67a0371884a016bb99ddf4

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,6 +2,6 @@ require "spec"
 require "../src/prism"
 
 def expect_vectors_match(got : Prism::Vector2f, expected : Prism::Vector2f)
-    got.x.should eq(expected.x)
-    got.y.should eq(expected.y)
+  got.x.should eq(expected.x)
+  got.y.should eq(expected.y)
 end
