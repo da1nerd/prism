346092514f61af555c2d71b860af32e20ecbd400
Joel <joel@neutrinographics.com>
2020-03-31 22:50:50 +0700

    added todos
    Former-commit-id: 3cee54924e5b1ec22db0696e568b27413d8221e2

--- src/prism/adapter/glfw/game_engine.cr
+++ src/prism/adapter/glfw/game_engine.cr
@@ -1,5 +1,10 @@
 require "prism-core"
 
+# TODO: this should be inside a GL namespace.
+# This is not tied to GLFW.
+# Actually, it would be nice if this wasn't tied to GL either, but was instad fully abstracted
+# from the implementations of the rendering engine. It should only receive a generic
+# rendering engine class. So this probably shouldn't be in adapters either.
 module Prism::Adapter::GLFW
   class GameEngine < Prism::Core::Engine
     def initialize(@game : Prism::Game, @rendering_engine : Prism::Adapter::GLFW::RenderingEngine)
