f9b7533b8a678cd5f078100333050efbc53820bd
Joel <joel@neutrinographics.com>
2020-04-09 23:53:10 +0700

    added optional color support to materials

--- src/prism/core/rendering/shader/shaders/forward-ambient.fs
+++ src/prism/core/rendering/shader/shaders/forward-ambient.fs
@@ -4,10 +4,11 @@ varying vec2 texCoord0;
 
 uniform vec3 R_ambient;
 uniform sampler2D diffuse;
+uniform vec3 materialColor;
 
 void main()
 {
-  vec4 color = texture2D(diffuse, texCoord0.xy) * vec4(R_ambient, 1);
+  vec4 color = (vec4(materialColor, 1.0) + texture2D(diffuse, texCoord0.xy)) * vec4(R_ambient, 1);
   // discards transparent pixels
   if (color.a <= 0.0) {
     discard;
