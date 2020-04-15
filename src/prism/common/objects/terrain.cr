9ef33faf430ea71a7bb8d6494a3742473864d5b7
Joel <joel@neutrinographics.com>
2020-04-15 16:13:48 +0700

    fixed height calculation for terrain

--- src/prism/common/objects/terrain.cr
+++ src/prism/common/objects/terrain.cr
@@ -1,66 +1,53 @@
 module Prism::Common::Objects
   class Terrain < Shape
-    SIZE         = 800
-    VERTEX_COUNT = 128
+    SIZE            = 800
+    MAX_HEIGHT      =  40
+    MAX_PIXEL_COLOR = 256 * 256 * 256 # because there are three color channels
 
     @x : Float32
     @z : Float32
 
-    def initialize(grid_x : Int32, grid_z : Int32)
+    def initialize(grid_x : Int32, grid_z : Int32, height_map : String)
       super()
       @x = (grid_x * SIZE).to_f32
       @z = (grid_z * SIZE).to_f32
-      generate_terrain
+      generate_terrain(height_map)
     end
 
-    def transform
-      @transform.pos = Vector3f.new(@x, 0, @z)
-      @transform
-    end
+    private def generate_terrain(height_map : String)
+      bitmap = Core::Bitmap.new(height_map)
 
-    private def generate_terrain
-      count : Int32 = (VERTEX_COUNT * VERTEX_COUNT).to_i32
-      #   verticies = StaticArray(Float32).new(count * 3)
-      #   normals = StaticArray(Float32).new(count * 3)
-      #   texture_coords = StaticArray(Float32).new(count * 2)
+      vertex_count = bitmap.height
+
+      count : Int32 = (vertex_count * vertex_count).to_i32
       vertices = [] of Core::Vertex
-      indices = [] of Int32 # .new(6 * (VERTEX_COUNT - 1) * (VERTEX_COUNT - 1))
-      # vertex_pointer = 0
-      0.upto(VERTEX_COUNT) do |i|
-        0.upto(VERTEX_COUNT) do |j|
+      indices = [] of Int32
+      0.upto(vertex_count - 1) do |i|
+        0.upto(vertex_count - 1) do |j|
           vertices.push(Core::Vertex.new(
             # vertex position
             Vector3f.new(
-              (j // (VERTEX_COUNT - 1) * SIZE).to_f32,
-              0f32,
-              (i // (VERTEX_COUNT - 1) * SIZE).to_f32
+              (j / (vertex_count - 1) * SIZE).to_f32,
+              get_height(j, i, bitmap),
+              (i / (vertex_count - 1) * SIZE).to_f32
             ),
             # texture coordinates
             Vector2f.new(
-              j.to_f32 // (VERTEX_COUNT - 1) * 40,
-              i.to_f32 // (VERTEX_COUNT - 1) * 40
+              j.to_f32 / (vertex_count - 1) * 40,
+              i.to_f32 / (vertex_count - 1) * 40
             ),
             # normals
-            Vector3f.new(0, 1, 0)
+            calculate_normals(j, i, bitmap)
           ))
-          # vertices[vertex_pointer*3] = j // (VERTEX_COUNT - 1) * SIZE
-          # vertices[vertex_pointer*3 + 1] = 0
-          # vertices[vertex_pointer*3 + 2] = i // (VERTEX_COUNT - 1) * SIZE
-          # normals[vertex_pointer*3] = 0
-          # normals[vertex_pointer*3 + 1] = 1
-          # normals[vertex_pointer*3 + 2] = 0
-          # texture_coords[vertex_pointer*2] = j // (VERTEX_COUNT - 1)
-          # texture_coords[vertex_pointer*2 + 1] = i // (VERTEX_COUNT - 1)
-          # vertex_pointer += 1
         end
       end
 
       pointer : Int32 = 0
-      0.upto(VERTEX_COUNT - 1) do |gz|
-        0.upto(VERTEX_COUNT - 1) do |gx|
-          top_left : Int32 = (gz * VERTEX_COUNT) + gx
+      0.upto(vertex_count - 1) do |gz|
+        0.upto(vertex_count - 1) do |gx|
+          top_left : Int32 = (gz * vertex_count) + gx
           top_right : Int32 = top_left + 1
-          bottom_left : Int32 = ((gz + 1)*VERTEX_COUNT) + gx
+          bottom_left : Int32 = ((gz + 1)*vertex_count) + gx
           bottom_right : Int32 = bottom_left + 1
           indices << top_left
           indices << bottom_left
@@ -72,6 +59,28 @@ module Prism::Common::Objects
       end
 
       @mesh = Core::Mesh.new(vertices, indices)
+    end
+
+    def calculate_normals(x : Int32, z : Int32, image : Core::Bitmap) : Vector3f
+      height_l = get_height(x - 1, z, image)
+      height_r = get_height(x + 1, z, image)
+      height_d = get_height(x, z - 1, image)
+      height_u = get_height(x, z + 1, image)
+
+      normal = Vector3f.new(height_l - height_r, 2f32, height_d - height_u)
+      normal.normalized
+    end
+
+    # Returns the height represented by a pixel in the image
+    private def get_height(x : Int32, z : Int32, image : Core::Bitmap) : Float32
+      return 0f32 if x < 0 || x >= image.height || z < 0 || z >= image.height
+
+      pixel = image.pixel(x, z)
+      height : Float32 = -((pixel.red.to_i32 << 16) + (pixel.green.to_i32 << 8) + pixel.blue.to_i32).to_f32
+      height += MAX_PIXEL_COLOR / 2f32
+      height /= MAX_PIXEL_COLOR / 2f32
+      height *= MAX_HEIGHT
+      height
     end
   end
 end
