8007841d5c6ab618c65f520897797f7d00435cad
Joel <joel@neutrinographics.com>
2020-05-01 09:07:04 +0700

    fixed formating
    Former-commit-id: d0ec47f60b0b17c1f438276dd5b008c72253c7c4

--- src/prism/input_dispatcher.cr
+++ src/prism/input_dispatcher.cr
@@ -1,14 +1,14 @@
 module Prism
-    # Dispatches input events to the rest of the components in an `Entity`
-    class InputDispatcher < Crash::Component
-        # Dispatches input events to the rest of the components on the *entity*
-        # This may modify the *entity* directly
-        def input!(tick : RenderLoop::Tick, input : RenderLoop::Input, entity : Crash::Entity)
-            entity.get_all.each do |component|
-                if component.is_a?(InputReceiver)
-                    component.input!(tick, input, entity)
-                end
-            end
+  # Dispatches input events to the rest of the components in an `Entity`
+  class InputDispatcher < Crash::Component
+    # Dispatches input events to the rest of the components on the *entity*
+    # This may modify the *entity* directly
+    def input!(tick : RenderLoop::Tick, input : RenderLoop::Input, entity : Crash::Entity)
+      entity.get_all.each do |component|
+        if component.is_a?(InputReceiver)
+          component.input!(tick, input, entity)
         end
+      end
     end
+  end
 end
--- src/prism/input_receiver.cr
+++ src/prism/input_receiver.cr
@@ -1,6 +1,6 @@
 module Prism
-    module InputReceiver
-        # Receives input events from the `InputDispatcher`
-        abstract def input!(tick : RenderLoop::Tick, input : RenderLoop::Input, entity : Crash::Entity)
-    end
+  module InputReceiver
+    # Receives input events from the `InputDispatcher`
+    abstract def input!(tick : RenderLoop::Tick, input : RenderLoop::Input, entity : Crash::Entity)
+  end
 end
