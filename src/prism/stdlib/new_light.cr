362b4217700ff70cfc8701e82955d1059cbb5407
Joel <joel@neutrinographics.com>
2020-05-12 00:18:29 +0700

    made light follow entity position
    Former-commit-id: 33e24bb017cf68657d6e30141b7ed3b3d84a5a0a

--- src/prism/stdlib/new_light.cr
+++ src/prism/stdlib/new_light.cr
@@ -1,17 +1,32 @@
 require "../light.cr"
+require "annotation"
 
 module Prism
   # An experiment with lights
   class NewLight < Prism::Light
+    include Prism::InputReceiver
+
     @[Prism::Shader::Field]
     @position : Vector3f
 
     @[Prism::Shader::Field]
     @color : Vector3f
 
-    property color, position
+    property color
+    getter position
 
-    def initialize(@position, @color)
+    # Creates a new light.
+    # The light will automatically follow the position of the entity.
+    # But you must add an `InputSubscriber` component to your entity in order for this to work.
+    # TODO: it would be nice to not have to add the subscriber component to the entity.
+    def initialize(@color)
+      @position = Vector3f.new(0,0,0)
+    end
+
+    @[Override]
+    def input!(tick : RenderLoop::Tick, input : RenderLoop::Input, entity : Crash::Entity)
+      # follow the entity
+      @position = entity.get(Prism::Transform).as(Prism::Transform).pos.clone
     end
   end
 end
