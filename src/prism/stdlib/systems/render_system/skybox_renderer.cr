a764c4ad439dcbc2224ae2cb0cd8a63c2dc83c7d
Joel <joel@neutrinographics.com>
2020-05-13 22:04:39 +0700

    updated todos
    Former-commit-id: 4e028b5298dffdfb093c37c3477b734a376d80f3

--- src/prism/stdlib/systems/render_system/skybox_renderer.cr
+++ src/prism/stdlib/systems/render_system/skybox_renderer.cr
@@ -1,64 +1,69 @@
 module Prism::Systems
   class SkyboxRenderer
-    SIZE     = 500f32
-    VERTICES = [
-      -SIZE, SIZE, -SIZE,
-      -SIZE, -SIZE, -SIZE,
-      SIZE, -SIZE, -SIZE,
-      SIZE, -SIZE, -SIZE,
-      SIZE, SIZE, -SIZE,
-      -SIZE, SIZE, -SIZE,
-
-      -SIZE, -SIZE, SIZE,
-      -SIZE, -SIZE, -SIZE,
-      -SIZE, SIZE, -SIZE,
-      -SIZE, SIZE, -SIZE,
-      -SIZE, SIZE, SIZE,
-      -SIZE, -SIZE, SIZE,
-
-      SIZE, -SIZE, -SIZE,
-      SIZE, -SIZE, SIZE,
-      SIZE, SIZE, SIZE,
-      SIZE, SIZE, SIZE,
-      SIZE, SIZE, -SIZE,
-      SIZE, -SIZE, -SIZE,
-
-      -SIZE, -SIZE, SIZE,
-      -SIZE, SIZE, SIZE,
-      SIZE, SIZE, SIZE,
-      SIZE, SIZE, SIZE,
-      SIZE, -SIZE, SIZE,
-      -SIZE, -SIZE, SIZE,
-
-      -SIZE, SIZE, -SIZE,
-      SIZE, SIZE, -SIZE,
-      SIZE, SIZE, SIZE,
-      SIZE, SIZE, SIZE,
-      -SIZE, SIZE, SIZE,
-      -SIZE, SIZE, -SIZE,
-
-      -SIZE, -SIZE, -SIZE,
-      -SIZE, -SIZE, SIZE,
-      SIZE, -SIZE, -SIZE,
-      SIZE, -SIZE, -SIZE,
-      -SIZE, -SIZE, SIZE,
-      SIZE, -SIZE, SIZE,
-    ]
-    # TEXTURE_FILES = ["right", "left", "top", "bottom", "back", "front"]
     @shader : Prism::SkyboxShader
     @cube : Prism::Model
+    @size : Float32 = 500
 
     def initialize(@shader : Prism::SkyboxShader)
-      @cube = Prism::Model.load_3f(VERTICES)
+      @cube = build_cube(@size)
+    end
+
+    private def build_cube(size : Float32)
+      Prism::Model.load_3f([
+        -size, size, -size,
+        -size, -size, -size,
+        size, -size, -size,
+        size, -size, -size,
+        size, size, -size,
+        -size, size, -size,
+
+        -size, -size, size,
+        -size, -size, -size,
+        -size, size, -size,
+        -size, size, -size,
+        -size, size, size,
+        -size, -size, size,
+
+        size, -size, -size,
+        size, -size, size,
+        size, size, size,
+        size, size, size,
+        size, size, -size,
+        size, -size, -size,
+
+        -size, -size, size,
+        -size, size, size,
+        size, size, size,
+        size, size, size,
+        size, -size, size,
+        -size, -size, size,
+
+        -size, size, -size,
+        size, size, -size,
+        size, size, size,
+        size, size, size,
+        -size, size, size,
+        -size, size, -size,
+
+        -size, -size, -size,
+        -size, -size, size,
+        size, -size, -size,
+        size, -size, -size,
+        -size, -size, size,
+        size, -size, size,
+      ] of Float32)
     end
 
     def render(entities : Array(Crash::Entity))
       # TODO: display a warning or something if more than one skybox is given
       if entities.size > 0
         skybox = entities[0].get(Skybox).as(Skybox)
-        # TODO: rebuild the cube if the size changes
+        if skybox.size != @size
+          # rebuild the cube when the size changes
+          @size = skybox.size
+          @cube = build_cube(@size)
+        end
         @shader.cube_map = skybox.texture
-        # draw cube
         @cube.bind
         offset = Pointer(Void).new(0)
         LibGL.draw_arrays(LibGL::TRIANGLES, 0, @cube.vertex_count)
